f6bf4d396d769cb569c42e5a5ab56e89e8607a1d
Brad Ramnarinesingh <45901683+Bradramnarinesingh@users.noreply.github.com>
2024-05-12 02:21:43 +0000

    Updates

---
+++ precache-manifest.b4e80d985d4be7fbafc9cd3071f1df24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab4ab84314f1d416bfbf80ca3f348c7",
+    "url": "/GradeTracker/index.html"
+  },
+  {
+    "revision": "9a609026158c178896b3",
+    "url": "/GradeTracker/static/css/main.b1b3450c.chunk.css"
+  },
+  {
+    "revision": "84a27a66a386613f33c0",
+    "url": "/GradeTracker/static/js/2.2618833d.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/GradeTracker/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "9a609026158c178896b3",
+    "url": "/GradeTracker/static/js/main.f07c0fc9.chunk.js"
+  },
+  {
+    "revision": "e4592665342ad138ab11",
+    "url": "/GradeTracker/static/js/runtime~main.c323d60e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/GradeTracker/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fefeafc2964b95a810f0096f2f3864fc",
+    "url": "/GradeTracker/static/media/setGoals.fefeafc2.webp"
+  },
+  {
+    "revision": "cece866f2488fd61aa51648cf5b30351",
+    "url": "/GradeTracker/static/media/trackGrades.cece866f.webp"
+  },
+  {
+    "revision": "2b7c686eb37de0ff1c57396dd50c993f",
+    "url": "/GradeTracker/static/media/visualize.2b7c686e.webp"
+  }
+]);
